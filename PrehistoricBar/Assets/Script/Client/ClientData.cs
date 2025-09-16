e29959983b8d818b85fa296d2522567f9d99a109
Shulkrex <alexthebasque@gmail.com>
2025-09-16 10:35:48 +0200

    Changement des nom de class

--- PrehistoricBar/Assets/Script/Client/ClientData.cs
+++ PrehistoricBar/Assets/Script/Client/ClientData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Script.Bar;
 
 [System.Serializable]
-public class ClientData
+public class ServiceData
 {
     public string name;
     public List<ClientClass> cocktails = new List<ClientClass>(2);
